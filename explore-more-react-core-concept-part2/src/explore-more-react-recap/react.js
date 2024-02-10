cf827f865bac019ed4621cd5eba4dc56fc355efb
Jerinislam05 <jerinislam05@gmail.com>
2024-02-10 15:46:13 +0000

    thinking in React way

---
+++ explore-more-react-core-concept-part2/src/explore-more-react-recap/react.js
@@ -0,0 +1,6 @@
+// Thinking in React way
+/* 
+1. Break it apart into pieces called components
+2. Describe the different visual states for each of the components
+3. Connect the components together so that the data flows through them.
+*/
