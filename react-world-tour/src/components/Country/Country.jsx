f679cc3203b94e0f7cce67c1e4dcc3cd3a78105b
Jerinislam05 <jerinislam05@gmail.com>
2024-02-14 13:22:22 +0000

    ADVANCED updating state in arrays and pass parameter

--- react-world-tour/src/components/Country/Country.jsx
+++ react-world-tour/src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css';
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
     const {name, flags, population, area, cca3} = country;
 
     const [visited, setVisited] = useState(false)
@@ -9,13 +9,17 @@ const Country = ({country}) => {
         setVisited(!visited)
     }
 
+    
+
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
+        <div  className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{color: visited? 'purple' : 'white'}}>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark visited</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'visited' : 'going'}</button>
             {visited ? 'I have visited this country.' : 'I want to visit.'}
         </div>
